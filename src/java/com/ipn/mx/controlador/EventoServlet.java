504aa1e489acbbb4e09378cd9ac08b747fa5b408
mikeni <mikeni@IWHBYD>
2018-05-04 15:09:44 -0500

    Se agrega el servlet y jsp

---
+++ src/java/com/ipn/mx/controlador/EventoServlet.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ipn.mx.controlador;
+
+import Conexiones.Conexion;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+/**
+ *
+ * @author mikeni
+ */
+
+@WebServlet(name="EventoServlet",urlPatterns={"/EventoServlet"})
+@MultipartConfig(maxFileSize=1024*100)
+public class EventoServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String nombreEvento=request.getParameter("nombreEvento");
+        int duracionEvento=Integer.parseInt(request.getParameter("duracionEvento"));
+        String fotoEvento=request.getParameter("fotoEvento");
+        
+        InputStream is=null;
+        Part archivo=request.getPart("fotoEvento");
+        if (archivo!=null) {
+            is=archivo.getInputStream();
+        }
+        try{
+            PreparedStatement ps = Conexion.prepareStatement(SQLINSERT);
+            ps.setString(1, nombreEvento);
+            ps.setInt(2, duracionEvento);
+            if (is!=null) {
+                ps.setBlob(3, is);
+            }
+            ps.executeUpdate();
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/vistaEvento.jsp
@@ -0,0 +1,44 @@
+<%-- 
+    Document   : vistaEvento
+    Created on : May 3, 2018, 2:10:30 AM
+    Author     : manuel
+--%>
+<%@page import="modelos.com.ipn.mx.modelo.Evento" %>
+
+<%@page import="java.util.*" %>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Vista Eventos</title>
+    </head>
+    <body>
+        <form method="POST" action="EventoServlet" enctype="multiport/form-data">
+            <table>
+                <tr>
+                    <th>Nombre Evento</th>
+                    <td>
+                        <input type="text" name="nombreEvento" placeholder="nombre del Evento" required="required"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="number" name="duracionEvento" placeholder="nombre del Evento" required="required"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="file" name="fotoEvento"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="submit" value="enviar" name="btnEnviar"/>
+                    </td>
+                </tr>
+            </table>          
+        </form>
+    </body>
+</html>
